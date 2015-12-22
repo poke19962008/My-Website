126badf608a627b7eef80cbc6b7d799fd8d17a51
Sayan Das <poke19962008@gmail.com>
2015-12-22 10:11:35 +0530

    added ScrollReveal and some template and CSS fixes

--- public/js/script.js
+++ public/js/script.js
@@ -16,24 +16,42 @@ $(document).ready(function (){
     method: 'GET',
   }).done(function (msg){
     $(".my_works_container").append(msg);
+    window.sr = ScrollReveal()
+                .reveal([
+                  ".my_works_title", ".my_works_desc",
+                  ".content_left", ".title_left", ".img_left"
+                ], {
+                  easing  : "ease-in",
+                  origin  : "left",
+                })
+
+                .reveal([
+                  ".my_works_title_img",
+                  ".content_right", ".title_right", ".img_right"
+                ], {
+                  easing  : "ease-in",
+                  origin  : "right"
+                });
+
+
   });
 
   window.sr = ScrollReveal()
-        .reveal("#slogan_dev", {
+        .reveal("#slogan_1", {
           delay    : 250*7 + 0,
           distance : '90px',
           easing   : 'ease-in',
           scale    : 1.1,
           mobile   : true,
         })
-        .reveal("#slogan_des", {
+        .reveal("#slogan_2", {
           delay    : 250*7 + 620,
           distance : '90px',
           easing   : 'ease-in',
           scale    : 1.1,
           mobile   : true,
         })
-        .reveal("#slogan_foo", {
+        .reveal("#slogan_3", {
           delay    : 250*7 + 1240,
           distance : '90px',
           easing   : 'ease-in',
@@ -46,5 +64,4 @@ $(document).ready(function (){
           easing   : 'ease-in-out',
           origin   : 'right',
         });
-
 });
